09508f582ff057849dae14057d8365538566abfc
v_ruicq <v_ruicq@fosun.com>
2018-07-27 18:26:09 +0800

    vue学习详解

---
+++ 53.php
@@ -0,0 +1,9 @@
+<?php 
+
+  $data = [
+    ['id' => 1,'title'=> 'iphon7Plus','price'=> 6999, 'num' => 3],
+    ['id' => 2,'title'=> 'iphon8Plus','price'=> 8999, 'num' => 5],
+  ];
+  echo json_encode($data);
+
+?>
